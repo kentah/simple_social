1d1bc30d841b3f23dc502e341aac4be0f4279625
kenthow@gmail.com <kenthow@gmail.com>
2019-07-13 18:00:41 -0500

    Created groups views and templates + urls. Created posts views + urls. Added django-braces

---
+++ groups/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import (LoginRequiredMixin, 
+        PermissionRequiredMixin)
+from django.urls import reverse
+from django.db import IntegrityError
+from django.views import generic
+
+from groups.models import Group, GroupMember
+from . import models
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
